70df8265bea1f6614b07763801775fdfde02e595
Raul <raul.teixeira@gmail.com>
2014-04-02 18:45:19 -0300

    Commit inicial

---
+++ server.js
@@ -0,0 +1,12 @@
+const
+    express = require('express'),
+    app = express();
+    
+app.use(express.logger('dev'));
+app.get('/api/:name', function(req, res) {
+    res.json(200, {"hello": req.params.name});
+});
+
+app.listen(3000, function() {
+        console.log('ready captain');
+    });
