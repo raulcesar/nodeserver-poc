e9bea5e94cffd1c30a99dc81b82433e341ca129f
Raul <raul.teixeira@gmail.com>
2014-04-04 17:52:46 -0300

    Acho que implementado script para pesquisar todos servidores no ldap.

--- testldap.js
+++ testldap.js
@@ -1,4 +1,6 @@
 var ldap = require('ldapjs');
+var fs = require('fs');
+var moment = require('moment');
 
 ldap.Attribute.settings.guid_format = ldap.GUID_FORMAT_B;
 
@@ -9,28 +11,85 @@ var client = ldap.createClient({
 
 });
 
+var filterStrPrefix='(&(mail=*@camara.leg.br)(title=Servidor Efetivo)';
+var filterStr = 'cn=p_';
 var opts = {
-  filter: '(cn=P_6486)',
-  scope: 'sub',
-  attributes: ['objectGUID', 'mail']
-},
-senha = process.argv[2];
+//  filter: '(cn=P_6486)',
+    //filter:'(&(OU=Usuarios)(mail=*@camara.leg.br))',
+    filter:'',
+  //scope: 'sub'
+  scope: 'sub'
+    //,
+//  attributes: ['objectGUID', 'mail']
+    },
+    senha = process.argv[2],
+    arq = process.argv[3];
 
+
+var vezesmax = process.argv[4] || 2;
+var arquivoUnico = process.argv[5] || false;
+var iniciovezes = process.argv[6] || 1;
+var searchescomplete = 0;
+var criaArquivo = function (vez) {
 client.bind('P_6486@redecamara.camara.gov.br', senha, function (err) {
     if (err) {
         console.log('erro ao fazer bind');
         console.log('erro especifico: ' + err.message);
         client.unbind(function(err) {
-            console.log('estou fazendo um unbind, pois ocorreu um erro.'); 
+            console.log('estou fazendo um unbind, pois ocorreu um erro: ' + err); 
         });
         return;
-        throw new Error(err.message);
     }
-  client.search('DC=redecamara, DC=camara, DC=gov, DC=br', opts, function (err, search) {
-    search.on('searchEntry', function (entry) {
-      var user = entry.object;
-      console.log(user);
-      client.unbind(function(err){}); 
-    });
-  });
+    var callbacksearch = function(err, search) {
+        search.on('searchEntry', function (entry) {
+            var objeto = entry.object;
+            var utc = parseInt(objeto.lastLogon)/10000;
+            var day = moment(utc);
+            var msg = objeto.cn+ ',' + 
+                objeto.displayName + ',' + 
+                objeto.givenName + ',' + 
+                objeto.sn + ',' + 
+                objeto.mail + ',' + 
+                objeto.telephoneNumber + ',' + 
+                day.format('DD/MM/YYYY - hh:mm:ss');
+
+            fs.appendFile(arquivo, msg + '\n', function(err) {
+                if (err) { 
+                    console.log('deu merda: ' + JSON.stringify(err));
+                } else {
+                    //console.log('inclui no arquivo: ' + msg);
+                }
+            });
+
+        });
+        search.on('end', function(result) {
+            searchescomplete ++;
+            console.log('searchescomplete: ' + searchescomplete);
+            console.log('vezesmax: ' + vezesmax);
+            if (vezesmax === searchescomplete) {
+                client.unbind(function(err){
+                    if (err) {
+                       console.log('erro no unbind: ' + JSON.stringify(err));
+                    }
+                });
+                console.log('unbinding: ');
+            }
+        });    
+    };
+
+
+    
+    opts.filter = filterStrPrefix + '(cn=p_' + vez + '*))';
+    console.log(opts.filter);
+    var arquivo = arq + '_' + vez + '.csv';
+
+    client.search('DC=redecamara, DC=camara, DC=gov, DC=br', opts, callbacksearch); 
 });
+};
+vezesmax += (iniciovezes - 1);
+for (var i = iniciovezes ; i <= vezesmax ; i++) {
+    console.log('i: ' + i);
+    criaArquivo(i);
+}
+
+
